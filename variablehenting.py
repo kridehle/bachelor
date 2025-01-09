38d3dbd561ecd9bd7267005f7b46bffb41022b0b
kridehle <kridehle@gmail.com>
2025-01-09 16:02:21 +0100

    delvis fungerende lesing av variabler

--- variablehenting.py
+++ variablehenting.py
@@ -1,20 +1,29 @@
+import ast
+
 
 def les_fra_fil():
     # Program to read the entire file using read() function
     file = open("variabler.txt", "r")
     content = file.read()
     file.close()
+    content = content.splitlines()
     return content
 
 def hent_ut_variabler():
     values = {}
     content = les_fra_fil()
-    #print(content)
+
     for line in content:
+
+        line = line.split("#", 1)[0].strip()
+
+        if not line:
+            continue
+
         line = line.strip()
         if "=" in line:
             name, value = line.split("=",1)
-            name = line.strip()
+            name = name.strip()
             value = value.strip()
             try:
                 value = eval(value)
